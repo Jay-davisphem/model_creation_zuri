7d9b572d59a7e72d207ca34080aeee72101d93a7
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-06-13 16:23:10 +0100

    initial commit

---
+++ I4G002603NAZ/blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+
+# to User for easy referencing
+User = get_user_model()
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return f"{self.title} - {self.published_date.date()}"
